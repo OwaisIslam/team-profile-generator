f32265f691b1ad59d2de5c510f6ad674b1d64708
Owais Islam <owaisislam@gmail.com>
2020-09-30 22:27:54 -0500

    trying to fix loop issue

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const generateHTML = require('./src/generateHTML');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+var manager = [];
+var team = [];
+
 const managerQuestions = [{
         type: 'input',
         name: 'managerName',
@@ -183,7 +186,7 @@ const internQuestions = [{
 ]
 
 const promptUser = () => {
-    return inquirer.prompt(managerQuestions)
+    return inquirer.prompt(managerQuestions);
 }
 
 const promptEngineer = teamData => {
@@ -191,7 +194,11 @@ const promptEngineer = teamData => {
     //     teamData.team = [];
     // }
 
-    return inquirer.prompt(engineerQuestions);
+    return inquirer.prompt(engineerQuestions)
+        .then(teamData => {
+            console.log("team data" + teamData);
+            checkAction(teamData);
+        })
 }
 
 const promptIntern = teamData => {
@@ -206,146 +213,42 @@ const endProgram = teamData => {
     return;
 }
 
-// const promptTeam = teamData => {
-//     if (!teamData.team) {
-//         teamData.team = [];
-//     }
-
-//     return inquirer.prompt(teamQuestions);
-// }
+const checkAction = choice => {
+    if (choice.action === 'Add an engineer') {
+        return promptEngineer();
+    } else if (choice.action === 'Add an intern') {
+        return promptIntern();
+    } else {
+        return teamData;
+    }
+}
 
 function start() {
     promptUser()
         .then(managerData => {
-            if (managerData.action == 'Add an engineer') {
+            manager = {
+                ...managerData
+            };
+            if (managerData.action === 'Add an engineer') {
                 return promptEngineer();
-            } else if (managerData.action == 'Add an intern') {
+            } else if (managerData.action === 'Add an intern') {
                 return promptIntern();
             } else {
                 return endProgram();
             }
         })
         .then(teamData => {
-            if (teamData.action == 'Add an engineer') {
-                return promptEngineer();
-            } else if (teamData.action == 'Add an intern') {
-                return promptIntern();
-            } else {
-                return endProgram();
-            }
+            checkAction(teamData);
+            // team.push(teamData);
+            // console.log(team);
+            // if (teamData.action === 'Add an engineer') {
+            //     return promptEngineer();
+            // } else if (teamData.action === 'Add an intern') {
+            //     return promptIntern();
+            // } else {
+            //     return teamData;
+            // }
         })
 }
 
 start();
-
-// const teamQuestions = [{
-//         type: 'list',
-//         name: 'action',
-//         message: 'Select an option:',
-//         choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
-//     },
-//     {
-//         type: 'input',
-//         name: 'engineerName',
-//         message: 'Enter the engineer name: ',
-//         validate: nameInput => {
-//             if (nameInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the engineer name!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'engineerID',
-//         message: 'Enter the engineer employee ID: (REQUIRED) ',
-//         validate: engineerIDInput => {
-//             if (engineerIDInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the engineer ID!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'engineerEmail',
-//         message: 'Enter the engineer email: (REQUIRED) ',
-//         validate: engineerEmailInput => {
-//             if (engineerEmailInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the engineer email!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'githubUsername',
-//         message: 'Enter the Github username: (REQUIRED) ',
-//         validate: githubUsernameInput => {
-//             if (githubUsernameInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the Github username!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'internName',
-//         message: 'Enter the intern name: ',
-//         validate: nameInput => {
-//             if (nameInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the intern name!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'internID',
-//         message: 'Enter the intern employee ID: (REQUIRED) ',
-//         validate: internIDInput => {
-//             if (internIDInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the intern ID!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'internEmail',
-//         message: 'Enter the intern email: (REQUIRED) ',
-//         validate: internEmailInput => {
-//             if (internEmailInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the intern email!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'school',
-//         message: 'Enter the intern school: (REQUIRED) ',
-//         validate: schoolInput => {
-//             if (schoolInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter the school!');
-//                 return false;
-//             }
-//         }
-//     }
-// ]
